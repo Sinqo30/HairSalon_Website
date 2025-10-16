fce3bddeb39ec5d0e89a9968ff0ba1a8433e5538
Sinqo30 <sinqo30@gmail.com>
2025-10-16 02:58:51 +0200

    Update functions.js
    added a comment

--- Website/Functions/functions.js
+++ Website/Functions/functions.js
@@ -1,3 +1,4 @@
 //This is the javascript file that should contain the functions of the site
 
-//This is Sinqo testing her ability to commit to GitHub, part 1.
+//This is Sinqo testing her ability to commit to GitHub, part 1
+//new comment
